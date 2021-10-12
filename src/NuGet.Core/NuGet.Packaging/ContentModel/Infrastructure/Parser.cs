b0be175fa8a0bf3efd34142c3693150987152e31
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-10-12 14:13:27 -0700

    add system.memory to nuget

--- src/NuGet.Core/NuGet.Packaging/ContentModel/Infrastructure/Parser.cs
+++ src/NuGet.Core/NuGet.Packaging/ContentModel/Infrastructure/Parser.cs
@@ -189,7 +189,7 @@ namespace NuGet.ContentModel.Infrastructure
                     {
                         break;
                     }
-                    var substring = path.Substring(startIndex, delimiterIndex - startIndex);
+                    var substring = path.AsSpan(startIndex, delimiterIndex - startIndex);
                     object value;
                     if (propertyDefinition.TryLookup(substring, _table, out value))
                     {
@@ -205,7 +205,7 @@ namespace NuGet.ContentModel.Infrastructure
                             }
                             if (StringComparer.Ordinal.Equals(_token, "tfm"))
                             {
-                                item.Properties.Add("tfm_raw", substring);
+                                item.Properties.Add("tfm_raw", substring.ToString());
                             }
                             item.Properties.Add(_token, value);
                         }
