09f10f8dc37fac4a2ec7bc4663a9b55462fa26de
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-11-27 21:32:48 -0800

    suppress warnings in apex tests project

---
+++ test/NuGet.Tests.Apex/NuGet.Console.TestContract/GlobalSuppressions1.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.ApexTestConsole.UIInvoke(System.Action)")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.ApexTestConsole.WaitForActionComplete(System.Action,System.TimeSpan)")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.NuGetApexConsoleTestService.GetPowershellConsole~System.Threading.Tasks.Task{NuGetConsole.Implementation.PowerConsoleToolWindow}")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.NuGetApexConsoleTestService.GetPowershellConsole~System.Threading.Tasks.Task{NuGetConsole.Implementation.PowerConsoleToolWindow}")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.NuGetApexConsoleTestService.GetWpfConsole~System.Threading.Tasks.Task{NuGetConsole.IWpfConsole}")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.Console.TestContract.NuGetApexConsoleTestService.GetWpfConsole~System.Threading.Tasks.Task{NuGetConsole.IWpfConsole}")]
---
+++ test/NuGet.Tests.Apex/NuGet.PackageManagement.UI.TestContract/GlobalSuppressions1.cs
@@ -0,0 +1,12 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.PackageManagement.UI.TestContract.ApexTestUIProject.UIInvoke(System.Action)")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.PackageManagement.UI.TestContract.ApexTestUIProject.UIInvoke``1(System.Func{``0})~``0")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.PackageManagement.UI.TestContract.ApexTestUIProject.WaitForActionComplete(System.Action,System.TimeSpan)~System.Boolean")]
+[assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.PackageManagement.UI.TestContract.ApexTestUIProject.WaitForSearchComplete(System.Action,System.TimeSpan)~System.Boolean")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:NuGet.PackageManagement.UI.TestContract.NuGetApexUITestService.GetProjectPackageManagerControl(System.String)~NuGet.PackageManagement.UI.PackageManagerControl")]
