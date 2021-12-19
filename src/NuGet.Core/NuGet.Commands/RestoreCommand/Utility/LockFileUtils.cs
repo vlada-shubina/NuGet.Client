9d1267e83ba4a40d19593c0cceb75604086f903a
Kartheek Penagamuri <kapenaga@microsoft.com>
2021-12-18 22:42:51 -0800

    use path.join in another case

--- src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/LockFileUtils.cs
+++ src/NuGet.Core/NuGet.Commands/RestoreCommand/Utility/LockFileUtils.cs
@@ -567,8 +567,11 @@ namespace NuGet.Commands
                     {
                         // If the project did not provide a list of assets, add in default ones.
                         // These are used to detect transitive vs non-transitive project references.
+#if NETCORE5_0
+                        var absolutePath = Path.Join(projectDir, "bin", "placeholder", $"{localMatch.Library.Name}.dll");
+#else
                         var absolutePath = Path.Combine(projectDir, "bin", "placeholder", $"{localMatch.Library.Name}.dll");
-
+#endif
                         files.Add(new ProjectRestoreMetadataFile(libAnyPath, absolutePath));
                     }
 
